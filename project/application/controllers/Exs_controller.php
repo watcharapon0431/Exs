c691ca3620d94f742975821b0f315789e9893595
watcharapon0431 <44535650+watcharapon0431@users.noreply.github.com>
2020-10-05 19:30:23 +0700

    exs

---
+++ project/application/controllers/Exs_controller.php
@@ -0,0 +1,52 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Exs_controller extends CI_Controller
+{
+
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->helper('date_helper');
+		$this->load->helper('guid_helper');
+		$this->load->helper('file_mime_type_helper');
+		$this->load->helper("file");
+		$this->load->helper('download');
+		$this->load->helper('url');
+	}
+
+	function output($body = '', $data = '')
+	{
+		// Start if condition when you not login before login
+		// Load header 
+		// $this->load->view('Template_Custom/v_header');
+		// Load Footer
+		// $this->load->view('Template_Custom/v_footer');
+		// Load Topbar
+		// $this->load->view('Template_Custom/v_topbar_reset');
+		// Load view and data is sent to view 
+		$this->load->view($body, $data);
+		// End if condition when you not login before login
+	}
+
+
+	/*
+	* Index site
+	* first site and load view v_home.php
+	* @author 
+	* @Create Date 
+	*/
+	function index()
+	{
+		// to view v_home 
+		$this->load_v_home();
+	}
+
+	
+	function load_v_home()
+	{
+		// to view v_home 
+		$this->output('v_home');
+	}
+
+}
